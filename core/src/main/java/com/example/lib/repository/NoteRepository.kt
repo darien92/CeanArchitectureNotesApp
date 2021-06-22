bf3375647729a6b7c1289c4c6452ee75e3801a3d
darien92 <darylgp92@gmail.com>
2021-06-22 13:04:43 -0500

    Repositories added

---
+++ core/src/main/java/com/example/lib/repository/NoteRepository.kt
@@ -0,0 +1,16 @@
+package com.example.lib.repository
+
+import com.example.lib.data.Note
+
+class NoteRepository(
+    private val dataSource: NoteDataSource
+)
+{
+    suspend fun addNote(note: Note) = dataSource.add(note)
+
+    suspend fun getNote(id: Long) = dataSource.get(id)
+
+    suspend fun getAllNotes() = dataSource.getAll()
+
+    suspend fun removeNote(note: Note) = dataSource.remove(note)
+}
