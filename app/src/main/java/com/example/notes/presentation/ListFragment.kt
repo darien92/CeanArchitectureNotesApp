642c977daa9027ef63e26cd952188c89598b57ab
darien92 <darylgp92@gmail.com>
2021-06-22 16:19:01 -0500

    Back navigation button added

--- app/src/main/java/com/example/notes/presentation/ListFragment.kt
+++ app/src/main/java/com/example/notes/presentation/ListFragment.kt
@@ -5,14 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.example.notes.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
+    private lateinit var fabAddNote: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabAddNote = view.findViewById(R.id.fab_add_note)
+        fabAddNote.setOnClickListener {
+            goToNoteDetails(0)
+        }
+    }
+
+    private fun goToNoteDetails(id:Long = 0L){
+        val action: NavDirections = ListFragmentDirections.actionListFragmentToNoteFragment(id)
+        Navigation.findNavController(requireView()).navigate(action)
     }
 }
--- app/src/main/java/com/example/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notes/presentation/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.notes.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.notes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //adding navigation on top
+        val navController = this.findNavController(R.id.fragment_container_view)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_container_view)
+        return NavigationUI.navigateUp(navController, null)
     }
 }
