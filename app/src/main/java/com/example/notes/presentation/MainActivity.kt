71358bdf83c1f79a9e7b0690b06ad04fc4766e89
darien92 <darylgp92@gmail.com>
2021-06-22 12:56:07 -0500

    New dependencies added and data layer created

--- app/src/main/java/com/example/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notes/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.notes
+package com.example.notes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ core/src/main/java/com/example/lib/data/Note.kt
@@ -0,0 +1,9 @@
+package com.example.lib.data
+
+data class Note(
+    val title: String,
+    val content: String,
+    val creationTime: Long,
+    val updateTime: Long,
+    val id: Long = 0
+)
