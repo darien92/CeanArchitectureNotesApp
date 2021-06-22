65d991daa2c79959c72afcac8c981ad915df2d8b
darien92 <darylgp92@gmail.com>
2021-06-22 16:57:24 -0500

    Room database added

---
+++ app/src/main/java/com/example/notes/framework/RoomNoteDataSource.kt
@@ -0,0 +1,19 @@
+package com.example.notes.framework
+
+import android.content.Context
+import com.example.lib.data.Note
+import com.example.lib.repository.NoteDataSource
+import com.example.notes.framework.db.DatabaseService
+import com.example.notes.framework.db.NoteEntity
+
+class RoomNoteDataSource(context: Context): NoteDataSource {
+    val noteDao = DatabaseService.getInstance(context).noteDao()
+    override suspend fun add(note: Note) = noteDao.addNoteEntity(NoteEntity.fromNoteToEntity(note))
+
+    override suspend fun get(id: Long): Note? = noteDao.getNoteEntity(id)?.toNoteFromEntity()
+
+    override suspend fun getAll(): List<Note> = noteDao.getAllNoteEntity().map { it.toNoteFromEntity() }
+
+    override suspend fun remove(note: Note) = noteDao.deleteNoteEntity(NoteEntity.fromNoteToEntity(note))
+
+}
