f93e6941039db197b6efbc008cde5ab29c743f17
darien92 <darylgp92@gmail.com>
2021-06-23 17:16:50 -0500

    Note List viewModel added

---
+++ app/src/main/java/com/example/notes/framework/viewmodels/ListViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.notes.framework.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.lib.data.Note
+import com.example.lib.repository.NoteRepository
+import com.example.lib.usecases.AddNote
+import com.example.lib.usecases.GetAllNotes
+import com.example.lib.usecases.GetNote
+import com.example.lib.usecases.RemoveNote
+import com.example.notes.framework.RoomNoteDataSource
+import com.example.notes.framework.UseCases
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ListViewModel(application: Application): AndroidViewModel(application) {
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+    private val repository = NoteRepository(RoomNoteDataSource(application))
+    private val useCases = UseCases(
+        AddNote(repository),
+        GetNote(repository),
+        GetAllNotes(repository),
+        RemoveNote(repository)
+    )
+    private val notes = MutableLiveData<List<Note>>()
+
+    fun getNote(){
+        coroutineScope.launch {
+            notes.postValue(useCases.getAllNotes())
+        }
+    }
+}
